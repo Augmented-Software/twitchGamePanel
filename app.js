0ce8b46e2833d9ad04edcbf6bb609546ea30eace
Richard Gordon <richgordon95@gmail.com>
2019-02-01 19:04:02 -0500

    begin scaffold game check logic

--- app.js
+++ app.js
@@ -11,3 +11,60 @@ const asyncMiddleware = fn => (req, res) => {
 router.get('/', asyncMiddleware(async (req, res) => {
   let
 }));
+
+
+/* the below line requests my account (retrieved the id already) and reveals the current set game*/
+// https://api.twitch.tv/kraken/channels/152033881/
+
+/*
+//if a game is returned from the request 
+if (user.game) {
+  //check if game is in list
+
+  switch(user.game) {
+    case gameList.game1:
+      console.log('game is already most recent game');
+      break;
+
+    case gameList.game2:
+    //bump game to top of list
+    break;
+
+    case gameList.game3:
+    //bump game to top of list
+    break;
+
+    case gameList.game4:
+    //bump game to top of list
+    break;
+
+    case gameList.game5:
+    //bump game to top of list
+    break;
+
+    case gameList.game6:
+    //bump game to top of list
+    break;
+
+    case gameList.game7:
+    //bump game to top of list
+    break;
+
+    case gameList.game8:
+    //bump game to top of list
+    break;
+
+    case gameList.game9:
+    //bump game to top of list
+    break;
+
+    case gameList.game10:
+    //bump game to top of list
+    break;
+
+    default:
+    //game is not on list so add it
+}
+
+};
+*/
